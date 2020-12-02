e87497cdff8a9af2e276410b47891d34c03ae483
Shah Rukh Khan <shah@crowdbotics.com>
2020-12-02 07:13:16 +0000

    Update models

--- test_88979789_app_21/wsgi.py
+++ test_88979789_app_21/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'test_88979789_app_21.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "test_88979789_app_21.settings")
 
 application = get_wsgi_application()
