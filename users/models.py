8913f2c8054941f7f2e5c35a26b9f6b7505be693
Shah Rukh Khan <shah@crowdbotics.com>
2020-12-21 11:31:26 +0000

    Update models

--- users/models.py
+++ users/models.py
@@ -25,3 +25,6 @@ class User(AbstractUser):
 
     def get_absolute_url(self):
         return reverse("users:detail", kwargs={"username": self.username})
+
+    def get_absolute_url(self):
+        return reverse("users:detail", kwargs={"username": self.username})
