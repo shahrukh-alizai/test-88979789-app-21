c9046ffe6d3a5eb756c74f413c28690c8e865f3a
Shah Rukh Khan <shah@crowdbotics.com>
2020-12-21 11:43:13 +0000

    Update models

--- home/models.py
+++ home/models.py
@@ -44,9 +44,28 @@ class CustomText(models.Model):
     def field(self):
         return "title"
 
+    def __str__(self):
+        return self.title
+
+    @property
+    def api(self):
+        return f"/api/v1/customtext/{self.id}/"
+
+    @property
+    def field(self):
+        return "title"
+
 
 class HomePage(models.Model):
     body = models.TextField()
+
+    @property
+    def api(self):
+        return f"/api/v1/homepage/{self.id}/"
+
+    @property
+    def field(self):
+        return "body"
 
     @property
     def api(self):
--- users/models.py
+++ users/models.py
@@ -28,3 +28,6 @@ class User(AbstractUser):
 
     def get_absolute_url(self):
         return reverse("users:detail", kwargs={"username": self.username})
+
+    def get_absolute_url(self):
+        return reverse("users:detail", kwargs={"username": self.username})
